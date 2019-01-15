a6e68aab7971440981c56671038612889160cd0f
avrech <avrech@campus.technion.ac.il>
2019-01-16 01:37:59 +0200

    adding learning curve, re-training, and statistics

--- wet2/dependency_parser.py
+++ wet2/dependency_parser.py
@@ -2,56 +2,170 @@ import pickle
 from tqdm import tqdm
 import numpy as np
 import datetime
-from chu_liu import Digraph
-
-
+from chu_liu_py2 import Digraph
+import matplotlib.pyplot as plt
+import time
+from tabulate import tabulate
 class DependencyParser:
-    def __init__(self, params):
-        self.true_graphs_dict = {}
-        self.digraphs_dict = {}
-        self.indexed_features = {}
-        self.local_features_dict = {}
-        self.global_features_dict = {}
-        self.params = params
-        self.num_of_features = 0
-        self.param_vec = None
+    def __init__(self, params, pre_trained_model_file=None):
+        """
+        Initialize the parser.
+        :param params: parameters for training.
+        :param record_rate: If > 0, evaluate train-set and test-set accuracy every record_rate epochs.
+        :param pre_trained_model_file: pickle file, contains the train_set, it's statistics dictionaries
+                           and the corresponding pre-trained param_vec.
+                           NOTE: if this parameter is not None, the params[train_file] is ignored,
+                           and the test-set is loaded from the pickle,
+                           in order to ensure compatibility of the param_vec and data.
+        """
+        # self.params = params
+        # self.load_model = pre_trained_model
+        self.test_set = read_anotated_file(params['test_file'])[:params['test_sentences_max']]
+        self.last_train_time = 0
+        if pre_trained_model_file is not None:
+            with open(pre_trained_model_file, 'rb') as f:
+                model = pickle.load(f)
+            self.true_graphs_dict     = model['true_graphs_dict']
+            self.digraphs_dict        = model['digraphs_dict']
+            self.indexed_features     = model['indexed_features']
+            self.local_features_dict  = model['local_features_dict']
+            self.global_features_dict = model['global_features_dict']
+            self.num_of_features      = model['num_of_features']
+            self.param_vec            = model['param_vec']
+            self.train_set            = model['train_set']
+            self.history              = model['history']
+            self.total_train_time     = model['total_train_time']
+            self.init_epoch           = model['final_epoch'] + 1
+            self.results              = model['results']
+        else:
+            self.true_graphs_dict = {}
+            self.digraphs_dict = {}
+            self.indexed_features = {}
+            self.local_features_dict = {}
+            self.global_features_dict = {}
+            self.num_of_features = 0
+            self.param_vec = None
+            self.train_set = read_anotated_file(params['train_file'])[:params['train_sentences_max']]
+            self.history = []
+            self.total_train_time = 0
+            self.init_epoch = 0
+            self.results = dict()
 
-    def train(self, sentences):
-        # calc for each sentence: the true graph, a feature vector for each (h,m) in x, and the digraph
-        for sentence in tqdm(sentences, 'Calculating train graphs...'):
-            true_graph = self.calc_graph(sentence)
-            self.true_graphs_dict[sentence] = true_graph
 
-        features_dict = self.extract_features(sentences)
-        curr_index = 0
-        for k in features_dict:
-            self.indexed_features[k] = {}
-            for feature in features_dict[k]:
-                self.indexed_features[k][feature] = curr_index
-                curr_index += 1
-        self.num_of_features = curr_index
-        self.param_vec = np.zeros(self.num_of_features)
+    def train(self, epochs=10, record_interval=0):
+        """
+        If load model is not None, the dictionaries assumed to be the same,
+        and there is no need to calculate them again.
+        Also, the param_vec get initial values from the pre-trained model,
+        and we
+        :return:
+        """
+        t_start = time.time()
+        if self.true_graphs_dict.__len__() == 0: # initialize dictionaries:
+            # calc for each sentence: the true graph, a feature vector for each (h,m) in x, and the digraph
+            for sentence in tqdm(self.train_set, 'Calculating train graphs...'):
+                true_graph = self.calc_graph(sentence)
+                self.true_graphs_dict[sentence] = true_graph
 
-        for sentence in tqdm(sentences, 'Calculating local and global features...'):
-            self.local_features_dict[sentence] = self.calc_local_features(sentence)
-            self.global_features_dict[sentence] = self.calc_global_features(sentence, self.true_graphs_dict[sentence])
-            self.digraphs_dict[sentence] = self.prepare_digraph(sentence)
+            features_dict = self.extract_features(self.train_set)
+            curr_index = 0
+            for k in features_dict:
+                self.indexed_features[k] = {}
+                for feature in features_dict[k]:
+                    self.indexed_features[k][feature] = curr_index
+                    curr_index += 1
+            self.num_of_features = curr_index
+            self.param_vec = np.zeros(self.num_of_features)
+
+            for sentence in tqdm(self.train_set, 'Calculating local and global features...'):
+                self.local_features_dict[sentence] = self.calc_local_features(sentence)
+                self.global_features_dict[sentence] = self.calc_global_features(sentence, self.true_graphs_dict[sentence])
+                self.digraphs_dict[sentence] = self.prepare_digraph(sentence)
 
         # run Perceptron to find best parameter vector
-        num_of_epochs = self.params['num_of_epochs']
+        # num_of_epochs = self.params['num_of_epochs']
+
+
+        print('')
         print('----------------------------------')
-        print('Running perceptron on sentences...')
+        print('Running perceptron on train_set...')
         print('----------------------------------')
-        for n in range(num_of_epochs):
-            for sentence in tqdm(sentences, 'Epoch no. {}'.format(n+1)):
+        for n in range(self.init_epoch, self.init_epoch + epochs):
+            for sentence in tqdm(self.train_set, 'Epoch no. {}'.format(n+1)):
                 y_pred = self.digraphs_dict[sentence].mst().successors
                 if y_pred != self.true_graphs_dict[sentence]:
                     new_w = self.param_vec + self.get_features_delta_vec(sentence, y_pred)
                     self.param_vec = new_w
+            # record history:
+            if record_interval > 0:
+                if np.mod(n, record_interval) == 0:
+                    train_acc = self.evaluate(self.train_set)
+                    test_acc = self.evaluate(self.test_set)
+                    self.history.append([n, train_acc, test_acc])
 
-        # Save parap_vec to pkl:
-        with open(str(datetime.datetime.now()) + "-param_vec.pkl", "wb") as f:
-            pickle.dump(self.param_vec, f)
+        self.last_train_time = time.time() - t_start
+        self.total_train_time += self.last_train_time
+        final_epoch = self.init_epoch + epochs
+        self.init_epoch += epochs
+
+        # Evaluate train-set and test-set
+        train_acc, train_eval_time = self.evaluate(self.train_set)
+        test_acc, test_eval_time = self.evaluate(self.test_set)
+
+        # update results
+        self.results['Train-set size'] = len(self.train_set)
+        self.results['Test-set size'] = len(self.test_set)
+        self.results['# features'] = self.num_of_features
+        self.results['# epochs'] = final_epoch
+        self.results['Last Training Time [minutes]'] = "{:.2f}".format(self.last_train_time / 60)
+        self.results['Total Training Time [minutes]'] = "{:.2f}".format(self.total_train_time / 60)
+        self.results['Test-set accuracy'] = "{:.2f}".format(test_acc)
+        self.results['Test-set evaluation time [minutes]'] = "{:.2f}".format(test_eval_time / 60)
+        self.results['Train-set accuracy'] = "{:.2f}".format(train_acc)
+        self.results['Train-set evaluation time [minutes]'] = "{:.2f}".format(train_eval_time / 60)
+
+        # Save model to pkl:
+        model_file_name = "saved_models/model_from-{}-trainset-{}-acc-{:.2f}-test_acc-{:.2f}.pkl".format(
+            datetime.datetime.now(),
+            len(self.train_set),
+            self.results['final_train_acc'],
+            self.results['final_test_acc'])
+
+        model = dict()
+        model['true_graphs_dict']     = self.true_graphs_dict
+        model['digraphs_dict']        = self.digraphs_dict
+        model['indexed_features']     = self.indexed_features
+        model['local_features_dict']  = self.local_features_dict
+        model['global_features_dict'] = self.global_features_dict
+        model['num_of_features']      = self.num_of_features
+        model['param_vec']            = self.param_vec
+        model['train_set']            = self.train_set
+        model['history']              = self.history
+        model['total_train_time']     = self.total_train_time
+        model['final_epoch']          = final_epoch
+        model['results']              = self.results
+
+        with open(model_file_name, "wb") as f:
+            pickle.dump(model, f)
+
+
+    def print_results(self):
+        print('-------------------------------------------------')
+        print('----------- Dependency-Parser Results -----------')
+        print('-------------------------------------------------')
+        print(tabulate(self.results.items(), headers=['Key', 'Value'], tablefmt='orgtbl', numalign='left'))
+
+    def plot_history(self):
+        if self.history.__len__() == 0:
+            print('There is no history to plot')
+            return
+        plt.figure(223)
+        hist = np.array(self.history)
+        plt.plot(hist)
+        plt.legend('train acc', 'test acc')
+        plt.title('Learning Curve')
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epochs')
 
 
     @staticmethod
@@ -212,11 +326,12 @@ class DependencyParser:
         """
         Evaluate accuracy of the model as (# true_predictions / # words)
         :param sentences: annotated sentences.
-        :return: accuracy in [0,1]
+        :return: accuracy in [0,1], inference time.
         """
+        t_start = time.time()
         total_shot = []
         for sentence in sentences:
-            true_successors = self.calc_graph(sentence)
+            true_successors = self.true_graphs_dict.get(sentence, self.calc_graph(sentence))
             pred_successors = self.infer(sentence)
             # calculate the head of each word in sentence
             true_deps = {c: p for p, children in true_successors.items() for c in children}
@@ -228,7 +343,7 @@ class DependencyParser:
                 print('actual:  ' + str(true_deps))
 
         acc = np.mean(total_shot)
-        return np.mean(acc)
+        return np.mean(acc), time.time()-t_start
 
 def read_anotated_file(filename):
     f = open(filename)
@@ -258,3 +373,26 @@ def measure_accuracy(dp, sentences):
         infered_dp = dp.infer(sentence)
         print('infered: ' + str(infered_dp))
         print('actual:  ' + str(dp.true_graphs_dict[sentence]))
+
+if __name__ == '__main__':
+    params = {
+        'train_file': 'train.labeled',
+        'train_sentences_max': None,
+        'test_sentences_max': None,
+        'test_file': 'test.labeled'
+    }
+
+    # train_set = read_anotated_file(params['train_file'])[:params['train_sentences_max']]
+    # test_set = read_anotated_file(params['test_file'])[:params['test_sentences_max']]
+
+    dp = DependencyParser(params, pre_trained_model_file=None)
+    dp.train(epochs=2, record_interval=1)
+    dp.plot_history()
+    dp.print_results()
+    # print('Evaluating test-set...')
+    # test_acc, test_eval_time = dp.evaluate(test_set)
+    #
+    # print('Evaluating train-set...')
+    # train_acc, train_eval_time = dp.evaluate(train_set)
+
+    print('finished'.format(datetime.datetime.now()))
--- wet2/ex2m1.py
+++ wet2/ex2m1.py
@@ -1,9 +1,8 @@
 import datetime
 import time
 from dependency_parser import read_anotated_file, DependencyParser, measure_accuracy
-from tabulate import tabulate
+
 params = {
-    'num_of_epochs': 100,
     'train_file': 'train.labeled',
     'train_sentences_max': None,
     'test_sentences_max': None,
@@ -12,33 +11,19 @@ params = {
 
 train_set = read_anotated_file(params['train_file'])[:params['train_sentences_max']]
 test_set = read_anotated_file(params['test_file'])[:params['test_sentences_max']]
-results = dict()
-results['Train-set size'] = len(train_set)
-results['Test-set size'] = len(test_set)
 
 dp = DependencyParser(params)
 print('Start training on {} samples...'.format(len(train_set)))
 t_start = time.time()
-dp.train(train_set)
-results['# features'] = dp.num_of_features
-results['# epochs'] = params['num_of_epochs']
-results['Training time [minutes]'] = "{:.2f}".format(time.time() - t_start/60)
+dp.train(epochs=3, record_interval=1)
 
 print('Evaluating test-set...')
-t_start = time.time()
-results['Test-set accuracy'] = "{:.2f}".format(dp.evaluate(test_set))
-results['Test-set evaluation time [minutes]'] = "{:.2f}".format(time.time() - t_start/60)
+test_acc, test_eval_time = dp.evaluate(test_set)
 
 print('Evaluating train-set...')
-t_start = time.time()
-results['Train-set accuracy'] = "{:.2f}".format(dp.evaluate(train_set))
-results['Train-set evaluation time [minutes]'] = "{:.2f}".format(time.time() - t_start/60)
+train_acc, train_eval_time = dp.evaluate(train_set)
 
 
 
-print('-------------------------------')
-print('----------- Results -----------')
-print('-------------------------------')
-print(tabulate(results.items(), headers=['Param', 'Val'], tablefmt='orgtbl', numalign='left'))
 
 print('finished'.format(datetime.datetime.now()))
