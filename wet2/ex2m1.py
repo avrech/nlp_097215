db994fb4aaa10a088ec2a136d3b4128b7cf07fc0
avrech <avrech@campus.technion.ac.il>
2019-01-18 00:27:09 +0200

    calculating confusion matrix

--- wet2/ex2m1.py
+++ wet2/ex2m1.py
@@ -9,23 +9,23 @@ if not os.path.isdir('saved_models'):
     os.mkdir('saved_models')
 params = {
     'train_file': 'train.labeled',
-    'train_sentences_max': 200,  # set to None to use full set
-    'test_sentences_max': 50,  # set to None to use full set
+    'train_sentences_max': None,  # set to None to use full set
+    'test_sentences_max': None,  # set to None to use full set
     'test_file': 'test.labeled',
     'threshold': {4: 2, 8: 2, 10: 2}  # set thresholds for features appearance.
     # a feature that appears less than th times is filtered.
 }
 
-if True:
+if False:
     # Choose None if to train a new model from scratch:
     model_file = None
 else:
     # Choose path if to continue training some pre-trained model, for example:
-    model_file = 'saved_models/2019-01-17/m100-test_acc-0.31-acc-0.72-from-00:31:10.pkl'
+    model_file = "saved_models/2019-01-17/m5000-test_acc-0.25-acc-0.29-from-22:50:17.pkl"
 
-epochs = 20  # total num of epochs
-snapshots = 10  # How many times to save model during training
-record_interval = 5  # evaluate model every num of epochs and store history for learning curve
+epochs = 1  # total num of epochs
+snapshots = 1  # How many times to save model during training (at least 1)
+record_interval = 0  # evaluate model every num of epochs and store history for learning curve
 eval_on = 20  # number of random samples to evaluate on.
 shuffle = True  # shuffle training examples every epoch
 # At the finale of every training session,
@@ -37,6 +37,7 @@ shuffle = True  # shuffle training examples every epoch
 
 dp = DependencyParser(params, pre_trained_model_file=model_file)
 dp.analyze_features()
+_, _, test_confusion_mat = dp.evaluate(dp.test_set, calc_confusion_matrix=True)
 for ii in range(snapshots):
     dp.train(epochs=np.ceil(epochs / snapshots),
              record_interval=record_interval,
